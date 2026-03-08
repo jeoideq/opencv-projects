39e087c11b396c3a6b49d1be4795ad203e976f7c
jeoideq <hanssonj20@gmail.com>
2026-03-08 13:05:46 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+print(sys.executable)
 import cv2
 
-img1=cv2.imread("lesson1/hippoimg.jpg",cv2.IMREAD_COLOR)
+img1=cv2.imread("hippoimg.jpg",cv2.IMREAD_GRAYSCALE) 
+# cv2.IMREAD_COLOR (1) to load the image in color 
+# cv2:IMREAD_GRAYSCALE (0) to load the image in greyscale
+# cv2.IMREAD_UNCHANGED (-1) to load the image unchanged
 cv2.imshow("hippo",img1)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
