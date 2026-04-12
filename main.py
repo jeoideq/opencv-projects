a21aad1d976933e406e55462ae8b23be076b220a
jeoideq <hanssonj20@gmail.com>
2026-04-12 13:04:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,36 @@
 import cv2
-import numpy as np
 
-hippo=cv2.imread("hippoimg.jpg",cv2.IMREAD_COLOR)
-cv2.imshow("hippo",hippo)
-cv2.waitKey(0)
-#using cvt color function to greyscale image
-new=cv2.cvtColor(hippo,cv2.COLOR_BGR2GRAY)
-cv2.imshow("new",new)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+forestimg=cv2.imread("forestimg.jpeg",cv2.IMREAD_COLOR)
+cv2.imshow("forestimg", forestimg)
+#draw a line on the image 
 
-#using averaging of pixels method to greyscale image
-(row,col)=hippo.shape[0:2]
-for i in range(row):
-    for j in range(col):
-        hippo[i,j]=sum(hippo[i,j].astype(np.uint16))//3
-cv2.imshow("average",hippo)
+stpoint=(1500,200)
+endpoint=(200,100)
+color=(28,16,199)
+thickness=50
+line=cv2.line(forestimg,stpoint,endpoint,color,thickness)
+cv2.imshow("line",forestimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#rotating an image
-hippo=cv2.imread("hippoimg.jpg",cv2.IMREAD_COLOR)
-cv2.imshow("hippo",hippo)
-M=cv2.getRotationMatrix2D((col/2,row/2),180,1.1)
-result=cv2.warpAffine(hippo,M,(col,row))
-cv2.imshow("rotate",result)
+#draw a circle
+center=(500,500)
+radius=400
+color=(0,0,0)
+thickness=-1
+circle=cv2.circle(forestimg,center,radius,color,thickness)
+cv2.imshow("circle",circle)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-#convert rgb to hsv
-newconvert=cv2.cvtColor(hippo,cv2.COLOR_BGR2HSV)
-cv2.imshow("hippo",newconvert)
+#draw a rectangle
+stpoint=(500,500)
+endpoint=(1500,150)
+color=(0,0,0)
+thickness=-1
+rectangle=cv2.rectangle(forestimg,stpoint,endpoint,color,thickness)
+cv2.imshow("rectangle",forestimg)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
-
-
 
 
