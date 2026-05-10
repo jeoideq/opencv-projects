caa1601d468cb45e8248d6f645726bd28b0dbc10
jeoideq <hanssonj20@gmail.com>
2026-05-10 13:02:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 import cv2
-from PIL import Image
-import os
+import numpy as np
+import time 
 
-path=r"lesson5/images"
-os.chdir(path)
-sumw=0
-sumh=0
-totalimg=len(os.listdir("."))
-for file in os.listdir("."):
-    img=Image.open(file)
-    width,height=img.size
-    sumw=sumw+width
-    sumh=sumh+height
+video1=cv2.VideoCapture(r"lesson6/projectvideo.mp4")
+time.sleep(1)
+count=0
+background=0
+for i in range(60):
+    return_value,background=video1.read()
+    if return_value==False:
+        continue
+background=np.flip(background,axis=1)
+while video1.isOpened():
+    return_value,image=video1.read()
+    if return_value==False:
+        break
+    count=count+1
+    image=np.flip(image,axis=1)
+    hsv=cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+    #range red color
+    lower_red=np.array([100,40,40])
+    upper_red=np.array([100,255,255])
+    mask1=cv2.inRange(hsv,lower_red,upper_red)
+    #range red color
+    lower_red=np.array([155,40,40])
+    upper_red=np.array([180,255,255])
+    mask2=cv2.inRange(hsv,lower_red,upper_red)
+    mask1=mask1+mask2
+    #for refining the image as the image is raw and blurry after processing the hsv format
+    mask1=cv2.morphologyEx(mask1,cv2.MORPH_OPEN,np.ones((3,3),np.uint8),iterations=2)
+    mask1=cv2.dilate(mask1,np.ones((3,3),np.uint8),iterations=1)
+    mask2=cv2.bitwise_not(mask1)
+    #applying the mask
+    result1=cv2.bitwise_and(background,background,mask=mask1)
+    result2=cv2.bitwise_and(image,image,mask=mask2)
+    finalresult=cv2.addWeighted(result1,1,result2,1,0)
+    cv2.imshow("invisableman",finalresult)
+    key=cv2.waitKey(10)
+    if key==27:
+        break
+    
 
-averagewidth=sumw/totalimg
-averageheight=sumh/totalimg
+                           
 
-for file in os.listdir("."):
-    if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png"):
-        img=Image.open(file)
-        newimg=img.resize([averagewidth,averageheight],Image.Resampling.LANCZOS)
-        newimg.save(file,"JPEG",quality=90)
-        print("resized")
 
-video1="video1.avi"
-images=[]
-for file in os.listdir("."):
-    if file.endswith(".jpeg") or file.endswith("jpg") or file.endswith(".png"):
-        images.append(file)
+    
 
-frame=cv2.imread(images[0])
-height,width,layers=frame.shape
-video=cv2.VideoWriter(video1,0,1,(width,height))
+    
+
         
+
+
+
+
 
 
 
