16380d7b156ab02bec7a8969fe598d884f950a54
jeoideq <hanssonj20@gmail.com>
2026-05-24 13:04:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 
 import os
  
-datasets="datasets"
+datasets=r"lesson7/datasets"
 sub_data="jade"
 path=os.path.join(datasets,sub_data)
 if not os.path.isdir(path):
@@ -10,7 +10,7 @@ if not os.path.isdir(path):
 #defining size of image 
 (width,height)=(130,100)
 face_cascade=cv2.CascadeClassifier("lesson7/cascade.xml")
-webcam=cv2.VideoCapture()
+webcam=cv2.VideoCapture(0)
 count=1
 while count <= 30:
     (_,im)=webcam.read()
@@ -18,8 +18,22 @@ while count <= 30:
     faces=face_cascade.detectMultiScale(grey,1.3,4)
     for face in faces:
         (x,y,w,h)=face
-        cv2.rectangle(im,(x,y),(x+w,y+h))
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
         face=grey[y:y+h,x:x+w]
+        face_resize=cv2.resize(face,(130,100))
+        cv2.imwrite("%s/%s.png"%(path,count),face_resize)
+    count =count+1
+    cv2.imshow("face_recognition",im)
+    key=cv2.waitKey(10)
+    if key==27:
+        break
+
+
+
+
+
+
+
         
 
 
